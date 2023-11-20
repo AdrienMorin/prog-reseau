dd1871d0f60a7c0486a35a8b00d88f4b8ab01f35
adrienmorin1 <adridu71170@gmail.com>
2023-11-20 08:18:24 +0100

    Reformat

--- socket_server.c
+++ socket_server.c
@@ -12,49 +12,60 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
-int main(int argc, char** argv )
-{ char datas[] = "hello\n";
-  int    sockfd,newsockfd,clilen,chilpid,ok,nleft,nbwriten;
-  char c;
-  struct sockaddr_in cli_addr,serv_addr;
-  
+int main(int argc, char **argv) {
+    char datas[] = "hello\n";
+    int sockfd, newsockfd, clilen, chilpid, ok, nleft, nbwriten;
+    char c;
+    struct sockaddr_in cli_addr, serv_addr;
 
-  if (argc!=2) {printf ("usage: socket_server port\n");exit(0);}
- 
-  printf ("server starting...\n");  
-  
-  /* ouverture du socket */
-  sockfd = socket (AF_INET,SOCK_STREAM,0);
-  if (sockfd<0) {printf ("impossible d'ouvrir le socket\n");exit(0);}
 
-  /* initialisation des parametres */
-  bzero((char*) &serv_addr, sizeof(serv_addr));
-  serv_addr.sin_family       = AF_INET;
-  serv_addr.sin_addr.s_addr  = htonl(INADDR_ANY);
-  serv_addr.sin_port         = htons(atoi(argv[1]));
+    if (argc != 2) {
+        printf("usage: socket_server port\n");
+        exit(0);
+    }
 
-  /* effecture le bind */
-  if (bind(sockfd,(struct sockaddr*)&serv_addr,sizeof(serv_addr))<0)
-     {printf ("impossible de faire le bind\n");exit(0);}
+    printf("server starting...\n");
 
-  /* petit initialisation */
-  listen(sockfd,1);
-     
-  /* attend la connection d'un client */
-  clilen = sizeof (cli_addr);
-  newsockfd = accept (sockfd,(struct sockaddr*) &cli_addr, &clilen);
-  if (newsockfd<0) {printf ("accept error\n"); exit(0);}
-  printf ("connection accepted\n");
+    /* ouverture du socket */
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        printf("impossible d'ouvrir le socket\n");
+        exit(0);
+    }
 
-  
-  while (1)
-   { while (read(newsockfd,&c,1)!=1);
-     printf("%c",c); 
-   }
-      
-   /*  attention il s'agit d'une boucle infinie 
-    *  le socket nn'est jamais ferme !
-    */
+    /* initialisation des parametres */
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(atoi(argv[1]));
 
-   return 1;
- }
+    /* effecture le bind */
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        printf("impossible de faire le bind\n");
+        exit(0);
+    }
+
+    /* petit initialisation */
+    listen(sockfd, 1);
+
+    /* attend la connection d'un client */
+    clilen = sizeof(cli_addr);
+    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+    if (newsockfd < 0) {
+        printf("accept error\n");
+        exit(0);
+    }
+    printf("connection accepted\n");
+
+
+    while (1) {
+        while (read(newsockfd, &c, 1) != 1);
+        printf("%c", c);
+    }
+
+    /*  attention il s'agit d'une boucle infinie
+     *  le socket nn'est jamais ferme !
+     */
+
+    return 1;
+}
